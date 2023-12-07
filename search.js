0a6837745f6435896ff68828b456a8778062e289
fu351 <afu7568@gmail.com>
2023-12-06 20:56:26 -0400

    Modified Search Endpoints

--- search.js
+++ search.js
@@ -1,8 +1,9 @@
 "use strict";
 const express = require('express');
-const multer = require('multer');
 const AWS = require('aws-sdk');
 const app = express();
+const semver = require('semver');
+
 
 
 AWS.config.update({
@@ -22,7 +23,7 @@ app.get('/package/{id}', (req, res) => {
     const packageID = req.body.packageID;
     const params = {
         Bucket: 'package-storage-1', //replace with bucket name
-        Key: {
+        Metadata: {
             packageID: packageID
         } 
     };
@@ -42,36 +43,106 @@ app.get('/package/byName', (req, res) => {
     const packageName = req.body.packageName;
     const params = {
         Bucket: 'package-storage-1', //replace with bucket name
-        Key: {
-            packageName: packageName
-        } 
     };
-    s3.getObject(params, (err, data) => {
-        if (err) {
-            console.error('Error retrieving file from S3:', err);
-            res.status(500).json({ error: 'Error downloading file from S3' });
-        } else {
-            res.status(200).json({ data });
-        }
-    });
+
+    try {
+        const data = await s3.listObjectsV2(params).promise();
+        const matchedPackages = data.Contents.filter(item => item.Key === packageName);
+
+        res.status(200).json({ data: matchedPackages });
+    } catch (err) {
+        console.error('Error retrieving files from S3:', err);
+        res.status(500).json({ error: 'Error downloading files from S3' });
+    }
 });
 //Search for a package using regular expression over package names and READMEs. Return the packages if the package name or the README matches the regular expression
 app.get('/package/byRegEx', (req, res) => {
     const regEx = req.body.regEx;
     const params = {
         Bucket: 'package-storage-1', //replace with bucket name
-        Key: {
-            packageName: regEx,
-            readMe: regEx
-        } 
     };
     //access the readme file and search for the regular expression
-    s3.getObject(params, (err, data) => {
-        if (err) {
-            console.error('Error retrieving file from S3:', err);
-            res.status(500).json({ error: 'Error downloading file from S3' });
-        } else {
-            res.status(200).json({ data });
+    try {
+        const data = await s3.listObjectsV2(params).promise();
+        const matchedPackages = [];
+
+        for (const item of data.Contents) {
+            const metadataParams = {
+                Bucket: params.Bucket,
+                Key: item.Key,
+            };
+
+            const metadata = await s3.headObject(metadataParams).promise();
+            const readme = metadata.Metadata.readme;
+
+            if (regEx.test(item.Key) || regEx.test(readme)) {
+                matchedPackages.push(item);
+            }
         }
-    });
+
+        res.status(200).json({ data: matchedPackages });
+    } catch (err) {
+        console.error('Error retrieving files from S3:', err);
+        res.status(500).json({ error: 'Error downloading files from S3' });
+    }
+});
+
+app.post('/packages', (req, res) => {
+    const packageName = req.body.packageName;
+    const versionInput = req.body.version;
+    const params = {
+        Bucket: 'package-storage-1', //replace with bucket name
+    };
+
+    try {
+        const data = await s3.listObjectsV2(params).promise();
+        const matchedPackages = [];
+
+        for (const item of data.Contents) {
+            if (item.Key === packageName) {
+                const metadataParams = {
+                    Bucket: params.Bucket,
+                    Key: item.Key,
+                };
+
+                const metadata = await s3.headObject(metadataParams).promise();
+                const version = metadata.Metadata.version;
+
+                if (versionInput.includes('-')) {
+                    const versionRange = versionInput.split('-').map(semver.clean);
+                    if (semver.gte(version, versionRange[0]) && semver.lte(version, versionRange[1])) {
+                        matchedPackages.push(item);
+                    }
+                } else if (versionInput.startsWith('~') || versionInput.startsWith('^')) {
+                    if (semver.satisfies(version, versionInput)) {
+                        matchedPackages.push(item);
+                    }
+                } else if (version === semver.clean(versionInput)) {
+                    matchedPackages.push(item);
+                }
+            }
+        }
+
+        res.status(200).json({ data: matchedPackages });
+    } catch (err) {
+        console.error('Error retrieving files from S3:', err);
+        res.status(500).json({ error: 'Error downloading files from S3' });
+    }
+});
+app.get('/directory', (req, res) => {
+    const pageNumber = Number(req.query.pageNumber) || 1;
+    const pageSize = Number(req.query.pageSize) || 100;
+    const params = {
+        Bucket: 'package-storage-1', //replace with bucket name
+        MaxKeys: pageSize,
+        ContinuationToken: pageNumber > 1 ? String(pageNumber) : undefined,
+    };
+
+    try {
+        const data = await s3.listObjectsV2(params).promise();
+        res.status(200).json({ data });
+    } catch (err) {
+        console.error('Error retrieving files from S3:', err);
+        res.status(500).json({ error: 'Error downloading files from S3' });
+    }
 });
