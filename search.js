853db18aa9a177d9b2967270e27f14f4d225432b
Ubuntu <ubuntu@ip-172-31-34-86.ec2.internal>
2023-12-10 03:54:32 +0000

    fixed imports

--- search.js
+++ search.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const AdmZip = require('adm-zip');
 const AWS = require('aws-sdk');
+const multer = require('multer');
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
