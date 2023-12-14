2dffe385ae54ddde62873077e44c63150a2bf3f3
cgcolquitt <89712044+cgcolquitt@users.noreply.github.com>
2023-12-14 02:21:09 -0500

    try again reset

--- reset.js
+++ reset.js
@@ -13,37 +13,33 @@ AWS.config.update({
 router.delete('/reset', async (req, res) => {
     const s3 = new AWS.S3();
     const bucketName = '461testbucket'; // Replace with your S3 bucket name
-    
+    const folderPrefix = 'packages/'; // Replace with the desired folder prefix
+
     try {
-        // List all objects in the bucket
-        const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
-    
-        // Check if there are any objects in the bucket
+        // List all objects in the specified folder
+        const data = await s3.listObjectsV2({ Bucket: bucketName, Prefix: folderPrefix }).promise();
+
+        // Check if there are any objects in the folder
         if (data.Contents.length === 0) {
             return res.status(200).json({ message: 'Registry is already empty' });
         }
-    
+
         // Prepare the list of objects to be deleted
         const objectsToDelete = data.Contents.map(obj => ({ Key: obj.Key }));
-    
-        // Delete the objects
-        // await s3.deleteObjects({
-        //     Bucket: '461testbucket',
-        //     Delete: { Objects: objectsToDelete },
-        // }).promise();
 
-        const deleteResponse = await s3.deleteObjects({
+        // Delete the objects
+        await s3.deleteObjects({
             Bucket: bucketName,
             Delete: { Objects: objectsToDelete },
         }).promise();
-        console.log('Delete response:', deleteResponse);
-    
+
         res.status(200).json({ message: 'Registry is reset.' });
     } catch (err) {
-        console.error('Error emptying S3 bucket:', err);
+        console.error('Error emptying S3 folder:', err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-    
+
 module.exports = router;
+
 
