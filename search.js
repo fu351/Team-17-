9bf9e154d60fa7ebac95dfc4f21b8d342e79a6a7
fu351 <122320084+fu351@users.noreply.github.com>
2023-12-14 03:46:48 -0500

    Directory view and search fix

--- search.js
+++ search.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const AdmZip = require('adm-zip');
 const AWS = require('aws-sdk');
+const { version } = require('winston');
 require('dotenv').config();
 
 AWS.config.update({
@@ -33,6 +34,7 @@ router.get('/package/:id', (req, res) => {
                         Name: data.Metadata.name,
                         Version: data.Metadata.version,
                         ID: data.Metadata.id,
+                        
                         },
                     data: {
                         Content: data.Body.toString('base64'),
@@ -72,7 +74,7 @@ router.get('/package/byName/:name', async (req, res) => {
             const params = {
                 Bucket: '461testbucket', //replace with bucket name
                 Prefix: `logs`,
-                Key: item.Key
+                Key: item.key
             };
             const logData = await s3.getObject(params).promise();
             return JSON.parse(logData.Body.toString('utf-8'));
@@ -106,8 +108,11 @@ router.post('/package/byRegEx', async (req, res) => {
 
             const object = await s3.getObject(metadataParams).promise();
             if (regEx.test(object.Metadata.Name)) {
-                const [Name, Version] = [object.Metadata.Name, object.Metadata.Version];
-                matchedPackages.push({ Name, Version });
+                const data = {};
+                    data.Name = object.Metadata.name;
+                    data.Version = object.Metadata.version;
+                    data.Popularity = object.Metadata.popularity;
+                    matchedPackages.push(data);
             }
             else {
             const zip = new AdmZip(object.Body.buffer);
@@ -116,8 +121,11 @@ router.post('/package/byRegEx', async (req, res) => {
             const readme = readmeEntry ? readmeEntry.getData().toString('utf8') : '';
             
                 if (regEx.test(readme)) {
-                    const [Name, Version] = [object.Metadata.Name, object.Metadata.Version];
-                    matchedPackages.push({ Name, Version });
+                    const data = {};
+                    data.Name = object.Metadata.name;
+                    data.Version = object.Metadata.version;
+                    data.Popularity = object.Metadata.popularity;
+                    matchedPackages.push(data);
                 }
             }
 
@@ -131,75 +139,123 @@ router.post('/package/byRegEx', async (req, res) => {
 });
 
 router.post('/packages', async (req, res) => {
-    const packageName = req.body.Name;
-    console.log(req.body);
-    const versionInput = req.body.Version;
-    const offset = req.query.offset || 0; // Get the offset from the query parameters, default to 0
-    
-    try {
-
-        if (!packageName) {
-            throw new Error('Package name is required');
+    const matchedPackages = [];
+    for (data in req.body ){
+        const packageName = data.Name;
+        console.log(req.body);
+        const versionInput = data.Version;
+        const offset = req.query.offset || 0; // Get the offset from the query parameters, default to 0
+        if (offset < 0) {
+            return res.status(400).json("Offset must be greater than or equal to 0.");
         }
-        if (packageName == "*") {
-            const params = {
-                Bucket: '461testbucket', //replace with bucket name
-                Prefix: `packages/`,
-                StartAfter: offset, // Start listing after the package name
-                MaxKeys: 100, // Return a maximum of 100 packages, prevents DOS attacks
-            }; 
-            const data = await s3.listObjectsV2(params).promise();
-            const matchedPackages = [];
-            for (const item of data.Contents) {
-                const metadataParams = {
-                    Bucket: '461testbucket',
-                    Key: item.Key,
-                };
-                const metadata = await s3.headObject(metadataParams).promise();
-                const [Name, Version] = [metadata.Metadata.Name, metadata.Metadata.Version, metadata.Metadata.ID];
-                matchedPackages.push({ Name, Version });
+        try {
+
+            if (!packageName) {
+                throw new Error('Package name is required');
             }
-
-        }
-        else {
-            const params = {
-                Bucket: '461testbucket', //replace with bucket name
-                Prefix: `packages/`,
-                StartAfter: offset, // Start listing after the package name
-                MaxKeys: 100, // Return a maximum of 100 packages, prevents DOS attacks
+            if (packageName == "*") {
+                const params = {
+                    Bucket: '461testbucket', //replace with bucket name
+                    Prefix: `packages/`,
+                    StartAfter: offset, // Start listing after the package name
+                    MaxKeys: 100, // Return a maximum of 100 packages, prevents DOS attacks
+                }; 
+                const data = await s3.listObjectsV2(params).promise();
                 
-            }; 
-            const data = await s3.listObjectsV2(params).promise();
-            const matchedPackages = [];
+                for (const item of data.Contents) {
+                    const metadataParams = {
+                        Bucket: '461testbucket',
+                        Key: item.Key,
+                    };
+                    const metadata = await s3.headObject(metadataParams).promise();
+                    //json object to be returned
+                    const data = {};
+                    data.Name = metadata.Metadata.name;
+                    data.Version = versionInput || metadata.Metadata.version;
+                    data.ID = metadata.Metadata.id;
+                    data.Popularity = metadata.Metadata.popularity;
+                    if (versionInput) {
+                        if (versionInput.includes('-')) {
+                            const versionRange = versionInput.split('-').map(semver.clean);
+                            if (semver.gte(version, versionRange[0]) && semver.lte(version, versionRange[1])) {
+                                matchedPackages.push(data);
+                            }
+                        } else if (versionInput.startsWith('~') || versionInput.startsWith('^')) {
+                            if (semver.satisfies(version, versionInput)) {
+                                matchedPackages.push(data);
+                            }
+                        } else if (version === semver.clean(versionInput)) {
+                            matchedPackages.push(data);
+                        }
+                        
+                    } else{
+                        matchedPackages.push(data);
+                    }
+                    if (matchedPackages.length >= 100) {
+                        return res.status(413).json("Too many packages returned.");
+                    }
 
-            for (const item of data.Contents) {
-                const metadataParams = {
-                    Bucket: params.Bucket,
-                    Key: item.Key,
-                };
-                const metadata = await s3.headObject(metadataParams).promise();
-                const version = metadata.Metadata.version;
-                if (metadata.Metadata.Name == packageName) {
-                    if (versionInput.includes('-')) {
-                        const versionRange = versionInput.split('-').map(semver.clean);
-                        if (semver.gte(version, versionRange[0]) && semver.lte(version, versionRange[1])) {
-                            matchedPackages.push(item);
+
+                }
+
+            }
+            else {
+                const params = {
+                    Bucket: '461testbucket', //replace with bucket name
+                    Prefix: `packages/`,
+                    StartAfter: offset, // Start listing after the package name
+                    MaxKeys: 100, // Return a maximum of 100 packages, prevents DOS attacks
+                    
+                }; 
+                const data = await s3.listObjectsV2(params).promise();
+                const matchedPackages = [];
+
+                for (const item of data.Contents) {
+                    const metadataParams = {
+                        Bucket: params.Bucket,
+                        Key: item.Key,
+                    };
+                    const metadata = await s3.headObject(metadataParams).promise();
+                    const version = metadata.Metadata.version;
+                    //json object to be returned
+                    const data = {};
+                    data.Name = metadata.Metadata.name;
+                    data.Version = versionInput || metadata.Metadata.version;
+                    data.ID = metadata.Metadata.id;
+                    data.Popularity = metadata.Metadata.popularity;
+                    if (metadata.Metadata.Name == packageName) {
+                        if (versionInput) {
+                            if (versionInput.includes('-')) {
+                                const versionRange = versionInput.split('-').map(semver.clean);
+                                if (semver.gte(version, versionRange[0]) && semver.lte(version, versionRange[1])) {
+                                    matchedPackages.push(data);
+                                }
+                            } else if (versionInput.startsWith('~') || versionInput.startsWith('^')) {
+                                if (semver.satisfies(version, versionInput)) {
+                                    matchedPackages.push(data);
+                                }
+                            } else if (version === semver.clean(versionInput)) {
+                                matchedPackages.push(data);
+                            }
+                            
+                        } else{
+                            matchedPackages.push(data);
                         }
-                    } else if (versionInput.startsWith('~') || versionInput.startsWith('^')) {
-                        if (semver.satisfies(version, versionInput)) {
-                            matchedPackages.push(item);
-                        }
-                    } else if (version === semver.clean(versionInput)) {
-                        matchedPackages.push(item);
+                    }
+                    if (matchedPackages.length >= 100) {
+                        return res.status(413).json("Too many packages returned.");
                     }
                 }
+
             }
-    
+            if (matchedPackages.length === 0) {
+                return res.status(404).json("No packages found. Please try again.");
+            }
+            res.status(200).json(matchedPackages);
+        } catch (err) {
+            console.error('Error retrieving files from S3:', err);
+            res.status(500).json({ error: 'Error downloading files from S3' });
         }
-        res.status(200).json({ data: matchedPackages });
-    } catch (err) {
-        console.error('Error retrieving files from S3:', err);
-        res.status(500).json({ error: 'Error downloading files from S3' });
     }
 });
 
