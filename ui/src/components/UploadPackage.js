35b60a82f3f55e023cf67f5a2e7c2083e3eefbba
Shreya Rathi <72522489+shreya-rathi@users.noreply.github.com>
2023-12-15 12:09:25 -0500

    final

--- ui/src/components/UploadPackage.js
+++ ui/src/components/UploadPackage.js
@@ -3,11 +3,40 @@ import React, { useState } from 'react';
 const UploadPackage = () => {
   const [packageName, setPackageName] = useState('');
   const [file, setFile] = useState(null);
+  const [b64file, setb64file] = useState(null);
 
   const handleFileChange = (e) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
+      getBase64(e.target.files[0]).then((result) => {
+        console.log("base 64 result");
+        console.log(result);
+        setb64file(result);
+        // console.log("File Is", result);
+      })
     }
+  };
+
+  const getBase64 = (file) => {
+    return new Promise(resolve => {
+      let fileInfo;
+      let baseURL = "";
+      // Make new FileReader
+      let reader = new FileReader();
+
+      // Convert the file to base64 text
+      reader.readAsDataURL(file);
+
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        console.log("Called", reader);
+        baseURL = reader.result;
+        console.log(baseURL);
+        resolve(baseURL);
+      };
+      return fileInfo;
+    });
   };
 
   const handleUpload = async () => {
@@ -15,13 +44,18 @@ const UploadPackage = () => {
       console.log("Uploading file...");
 
       const formData = new FormData();
-      formData.append("package", file);
+      formData.append("file", file);
       formData.append('name', packageName);
+      formData.append("Content", b64file);
 
       try {
         const result = await fetch("http://3.12.123.204:3000/package", {
           method: "POST",
           body: formData,
+          headers: {
+            // Add your X-Authorization header here with the appropriate token value
+            'x-authorization': '0',
+          },
         });
 
         const data = await result.json();
